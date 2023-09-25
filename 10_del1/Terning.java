3c70b5c19119c9fae359d2dd1edc5dfcd4177262
Vittrup02 <tobias2311@gmail.com>
2023-09-25 16:12:21 +0200

    This code will generate a random value between 1-6 for the two dices and prints the values out in the console.

---
+++ 10_del1/Terning.java
@@ -0,0 +1,11 @@
+package test;
+import spil.Terning;
+
+class Terning {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
---
+++ DiceRoll.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+public class DiceRoll {
+
+    private int Dice_1;
+    private int Dice_2;
+
+    public DiceRoll() {
+        // Create a Random object
+        Random random = new Random();
+
+        // Simulate rolling two dice when creating the object
+        Dice_1 = random.nextInt(6) + 1;
+        Dice_2 = random.nextInt(6) + 1;
+    }
+
+    public int getDice_1() {
+        return Dice_1;
+    }
+
+
+    public int getDice_2() {
+        return Dice_2;
+    }
+
+    public static void main(String[] args) {
+        DiceRoll roll = new DiceRoll();
+
+        System.out.println("Dice_1: " + roll.getDice_1());
+        System.out.println("Dice_2: " + roll.getDice_2());
+        
+    }
+}
+
