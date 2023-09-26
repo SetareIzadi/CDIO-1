2811e9df9d7110f02d719e2d2008482bed2a6f10
Dennis Dogulu <eren@hotmail.dk>
2023-09-26 15:57:08 +0200

    Fixed file

---
+++ Dicegame/Main.java
@@ -0,0 +1,40 @@
+package Dicegame;
+
+public class Main{
+
+public static void main(String[] args) {
+
+
+fin
+
+
+while(Player1.getScore()<40 && Player2.getScore<40){
+
+    // Player 1 loop first
+    System.out.println("__________________");
+    System.out.println("Player 1 goes first");
+
+    if(roll.checkIfEqual()){
+    roll.scoreFace();
+    Player1.sumScore(roll.getScore());
+    System.out.println(player1.toStringScore());
+
+    } else {
+    (roll.checkIfDoubleone()){
+    System.out.println(player1.toStringTwoOnes);
+    player1.startOver(true);
+    System.out.println(player1.toStringScore());
+
+}
+}
+
+
+
+
+
+
+
+// player 2 loop second
+}
+}
+}
