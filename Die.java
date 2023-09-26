859230fa911344e237c5e692d6c6149f6c90576d
Vittrup02 <tobias2311@gmail.com>
2023-09-26 15:15:49 +0200

    DieRoll This code will give a die a facevalue when called

---
+++ Die.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Die {
+    private int faceValue;
+    private Random random = new Random();
+
+    public int roll() {
+        faceValue = random.nextInt(6) + 1;
+
+        return faceValue;
+    }
+}
+
+
