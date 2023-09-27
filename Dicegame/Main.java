92c177c2961bd165597e5e107bed4863b14341bc
Vittrup02 <tobias2311@gmail.com>
2023-09-27 14:31:01 +0200

    Some more code to the main game that should tell if player one wins the game?

--- Dicegame/Main.java
+++ Dicegame/Main.java
@@ -22,46 +22,38 @@ while(player1.getScore()<40 && player2.getScore()<40){
     System.out.println(player1.toStringTotalScore());
 
  } else{
- } 
-    if(roll.checkIfDoubleone()){
-            System.out.println("Too bad you rolled two ones and have to start over :( )");
-            player1.startOver(true);    
-            System.out.println(player1.toStringTotalScore());
-
-} else{
-
-    while(roll.checkForDouble()){ //mulig fejl her
+        while(roll.checkForDouble()) //mulig fejl her
         roll.sumFace();
         player1.sumScore(roll.getSum());
         System.out.println("You rolled two of a kind!, you get an extra turn!");
         System.out.println(player1.toStringTotalScore());
         
-        if(roll.checkForNotSixOne()){
+
+        if(roll.checkIfDoubleone()){
+            System.out.println("Too bad you rolled two ones and have to start over :( )");
+            player1.startOver(true);    
+            System.out.println(player1.toStringTotalScore());
+            break;
+
+        }if(roll.checkForNotSixOne()){
             continue;
-        }
-
+        
+        } else if (roll.checkForTwoSix()){
+            player1.sumScore(40);
+        
+        } else {
+            roll.sumFace();
+            player1.sumScore(roll.getSum());
+            System.out.println(player1.toStringTotalScore());
     }
+    
 } 
-
-
-
+    if (player1.getScore() >= 40){
+        System.out.println("Congratulations, you won the Dicegame with: ");
     }
-}
-
- }
-    
-     
-
-    
-
+        
 }
 }
-
-
-
-
-
-
-
+}   
 // player 2 loop second
 
